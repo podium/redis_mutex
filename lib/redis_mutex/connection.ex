c3d6290c35f0e5a8b4561d5c7d1f3077b8d1fa96
Damon Janis <damon.janis@podium.com>
2022-11-02 14:35:30 -0600

    Fix child spec

--- lib/redis_mutex/connection.ex
+++ lib/redis_mutex/connection.ex
@@ -2,6 +2,17 @@ defmodule RedisMutex.Connection do
   @moduledoc """
   This module connects to the Redis instance.
   """
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, opts},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
   def start_link(name, uri) do
     Redix.start_link(uri, name: name)
   end
