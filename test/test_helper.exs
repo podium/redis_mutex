b632d0a67da47c42090d97bceb6ecc16389568f7
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-15 08:37:42 -0500

    Define mock for RedisMutex.LockV2

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,7 @@ ExUnit.configure(exclude: [:redis_dependent])
 ExUnit.start(exclude: [:skip])
 
 {:ok, files} = File.ls("./test/support")
+Mox.defmock(RedisMutex.LockV2Mock, for: RedisMutex.LockV2)
 
 Enum.each(files, fn file ->
   Code.require_file("support/#{file}", __DIR__)
