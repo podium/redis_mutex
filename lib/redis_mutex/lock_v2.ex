83abbfd90b3c7b5029e8d48630c82e6f6a09f959
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-15 10:52:23 -0500

    Add functions to LockV2 to implement its behaviours

--- lib/redis_mutex/lock_v2.ex
+++ lib/redis_mutex/lock_v2.ex
@@ -3,9 +3,19 @@ defmodule RedisMutex.LockV2 do
   Defines the lock behaviour for RedisMutex.
   """
 
-  @type start_options :: Keyword.t()
+  @default_timeout :timer.seconds(40)
+  @default_expiry :timer.seconds(20)
+  @unlock_script """
+  if redis.call("get", KEYS[1]) == ARGV[1] then
+    return redis.call("del", KEYS[1])
+  else
+    return 0
+  end
+  """
 
-  @callback child_spec(start_options :: start_options()) :: Supervisor.child_spec()
+  @type start_options :: String.t() | Keyword.t()
+
+  @callback child_spec(opts :: Keyword.t()) :: Supervisor.child_spec()
 
   @callback start_link(start_options :: start_options()) :: {:ok, pid()} | {:error, any()}
 
@@ -13,4 +23,88 @@ defmodule RedisMutex.LockV2 do
               do: clause :: term()
             ) ::
               any()
+
+  @spec child_spec(Keyword.t()) :: Supervisor.child_spec()
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, opts},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  @spec start_link(start_options()) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(start_options \\ []) do
+    Redix.start_link(start_options)
+  end
+
+  @spec with_lock(String.t(), integer(), integer(), do: term()) :: any()
+  defmacro with_lock(key, timeout \\ @default_timeout, expiry \\ @default_expiry, do: clause) do
+    quote do
+      key = unquote(key)
+      timeout = unquote(timeout)
+      expiry = unquote(expiry)
+      uuid = UUID.uuid1()
+
+      RedisMutex.LockV2.take_lock(key, uuid, timeout, expiry)
+
+      block_value = unquote(clause)
+
+      RedisMutex.Lock.unlock(key, uuid)
+
+      block_value
+    end
+  end
+
+  @doc """
+  This function takes in a key, unique string, and a timeout in milliseconds.
+  It will call itself recursively until it is able to set a lock
+  or the timeout expires.
+  """
+  def take_lock(key, uuid, timeout \\ @default_timeout, expiry \\ @default_expiry, finish \\ nil)
+
+  def take_lock(key, uuid, timeout, expiry, nil) do
+    finish = DateTime.add(DateTime.utc_now(), timeout, :millisecond)
+    take_lock(key, uuid, timeout, expiry, finish)
+  end
+
+  def take_lock(key, uuid, timeout, expiry, finish) do
+    if DateTime.compare(finish, DateTime.utc_now()) == :lt do
+      raise RedisMutex.Error, message: "Unable to obtain lock."
+    end
+
+    if !lock(key, uuid, expiry) do
+      take_lock(key, uuid, timeout, expiry, finish)
+    end
+  end
+
+  @doc """
+  This function takes in a key and a unique identifier to set it in Redis.
+  This is how a lock is identified in Redis. If a key/value pair is able to be
+  set in Redis, `lock` returns `true`. If it isn't able to set in Redis, `lock`
+  returns `false`.
+  """
+  def lock(key, value, expiry) do
+    # CHANGE THIS SO WE CAN FIND THE RIGHT PROCESS BASED ON THE WORKER
+    client = Process.whereis(:redis_mutex_connection)
+
+    case Redix.command!(client, ["SET", key, value, "NX", "PX", "#{expiry}"]) do
+      "OK" -> true
+      nil -> false
+    end
+  end
+
+  @doc """
+  This function takes in the key/value pair that are to be released in Redis
+  """
+  def unlock(key, value) do
+    client = Process.whereis(:redis_mutex_connection)
+
+    case Redix.command!(client, ["EVAL", @unlock_script, 1, key, value]) do
+      1 -> true
+      0 -> false
+    end
+  end
 end
