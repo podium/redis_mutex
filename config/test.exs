d2fe89361d2d2916defe4c89b8ddea5e3cda9383
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-18 10:12:59 -0500

    Include redis_url in test config

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,7 @@
 import Config
 
 config :redis_mutex, redis_url: "redis://localhost:6379"
-config :redis_mutex, lock_module: RedisMutex.LockMock
+
+config :redis_mutex, RedisMutexV2Test.RedisMutexUser,
+  lock_module: RedisMutex.LockV2Mock,
+  redis_url: "redis://localhost:6379"
