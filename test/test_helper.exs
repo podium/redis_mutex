31564644b3b9b0fa5e6779c49cb660d765d43809
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-18 10:23:05 -0500

    Remove test/support/lock.ex file

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,9 +1,4 @@
 ExUnit.configure(exclude: [:redis_dependent])
 ExUnit.start(exclude: [:skip])
 
-{:ok, files} = File.ls("./test/support")
 Mox.defmock(RedisMutex.LockV2Mock, for: RedisMutex.LockV2)
-
-Enum.each(files, fn file ->
-  Code.require_file("support/#{file}", __DIR__)
-end)
