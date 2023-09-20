ed2c43360e94b8b21e4df162e960f6de78f8e057
Clifton McIntosh <clifton.mcintosh@podium.com>
2023-09-20 08:40:35 -0500

    Adjust test helper to include all exclusions in the configure function

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-ExUnit.configure(exclude: [:redis_dependent])
-ExUnit.start(exclude: [:skip])
+ExUnit.start()
+ExUnit.configure(exclude: [:redis_dependent, :skip])
 
 Mox.defmock(RedisMutex.LockMock, for: RedisMutex.Lock)
