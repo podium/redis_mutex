0cc0f38dfda369a7a832bb6599262a2695443886
Clifton McIntosh <clifton.mcintosh@podium.com>
2024-02-07 09:13:46 -0600

    Remove start config from config files

--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,1 @@
 import Config
-
-config :redis_mutex, redis_url: "redis://localhost:6379"
--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,1 @@
 import Config
-
-config :redis_mutex, RedisMutex, redis_url: "redis://localhost:6379"
-
-config :redis_mutex, MyApp.RedisMutex,
-  host: "localhost",
-  port: 6379
